b18d15985eb72a8928f7befed17de0accfebae4d
Murielly-Nascimento <murielly.nascimento@ufu.br>
2021-06-02 18:54:09 -0300

    Prolog

--- Trabalho/backend/bd/tbl_curso.pl
+++
@@ -1,2 +0,0 @@
-created(1622033483.614522).
-assert(curso(1,'Ciencias da Computaçao','BCC')).
--- Trabalho/backend/bd/tbl_instituicao.pl
+++
@@ -1,2 +0,0 @@
-created(1622033904.204798).
-assert(instituicao(1,'Universidade Federal de Uberlandia','UFU','Uberlandia')).
